ebdc70c6011df06b133390118b54a4fb4c1f9aac
Gurung <sgurung@ALPHA.local>
2016-10-20 17:39:08 -0700

    render d3 and react component separately

--- project/static/scripts/js/main.js
+++ project/static/scripts/js/main.js
@@ -39,13 +39,8 @@ var DynamicSearch = React.createClass({displayName: "DynamicSearch",
 
 // list of countries, defined with JavaScript object literals
 var countries = [
-  {"name": "Sweden"}, {"name": "China"}, {"name": "Peru"}, {"name": "Czech Republic"},
-  {"name": "Bolivia"}, {"name": "Latvia"}, {"name": "Samoa"}, {"name": "Armenia"},
-  {"name": "Greenland"}, {"name": "Cuba"}, {"name": "Western Sahara"}, {"name": "Ethiopia"},
-  {"name": "Malaysia"}, {"name": "Argentina"}, {"name": "Uganda"}, {"name": "Chile"},
-  {"name": "Aruba"}, {"name": "Japan"}, {"name": "Trinidad and Tobago"}, {"name": "Italy"},
-  {"name": "Cambodia"}, {"name": "Iceland"}, {"name": "Dominican Republic"}, {"name": "Turkey"},
-  {"name": "Spain"}, {"name": "Poland"}, {"name": "Haiti"}
+  {"name": "Sweden"}, {"name": "China"}, {"name": "Czech Republic"}, {"name": "Cuba"},
+  {"name": "Japan"}, {"name": "Italy"}, {"name": "United States"}, {"name": "Great Britain"}
 ];
 
 ReactDOM.render(
---
+++ project/static/scripts/jsx/circles.js
@@ -0,0 +1,10 @@
+var Circles = React.createClass({
+  render: function() {
+    return (<h1>Hi!</h1>);
+  }
+});
+
+ReactDOM.render(
+  <Circles />,
+  document.getElementById('react-d3')
+);
